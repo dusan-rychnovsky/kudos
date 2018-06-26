672c0d0ac6d53f221c2e2870946c703121d6dc0c
Dusan Rychnovsky <durychno@microsoft.com>
2018-06-26 18:06:02 +0200

    Set page title.

--- rails/spec/features/creating_kudos_spec.rb
+++ rails/spec/features/creating_kudos_spec.rb
@@ -12,5 +12,11 @@ RSpec.feature "Users can create new Kudo" do
     click_button "Create Kudos"
 
     expect(page).to have_content "Kudos has been created."
+
+    kudo = Kudo.find_by(for: "Finally deciding to learn Rails.")
+    expect(page.current_url).to eq kudo_url(kudo)
+
+    title = "Kudos - Finally deciding to learn Rails."
+    expect(page).to have_title title
   end
 end
