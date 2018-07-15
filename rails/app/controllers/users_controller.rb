f2753773c96dfc56ac79457d8317829d7c46e3c9
Dusan Rychnovsky <durychno@microsoft.com>
2018-07-15 09:39:51 +0200

    Show Kudo#created_at.

--- rails/app/controllers/users_controller.rb
+++ rails/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @kudos = @user.kudos_to
+    @kudos = @user.kudos_to.order('created_at DESC')
   end
 end
